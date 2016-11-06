53254af8743960bd264e3186748b30c3620692a0
Mythri Garaga Manjunatha <mythrigm@gmail.com>
2016-11-06 17:49:11 -0500

    Update MergeSortedArrays.java

--- MergeSortedArrays.java
+++ MergeSortedArrays.java
@@ -1,8 +1,12 @@
-import java.util.Arrays;
+
 
 /**
  * Created by Mythri on 11/3/16.
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+   Note:You may assume that nums1 has enough space (size that is greater or equal to m + n) to 
+   hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
  */
+import java.util.Arrays;
 public class MergeSortedArrays {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m-1;
